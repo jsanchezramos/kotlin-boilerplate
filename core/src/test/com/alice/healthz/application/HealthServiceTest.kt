c3245259d669f2003d16d159b173ba90abf66133
juan.sanchez <juanfrancisco.sanchezramos@mytheresa.com>
2020-06-10 11:01:55 +0200

    change coverage core

---
+++ core/src/test/com/alice/healthz/application/HealthServiceTest.kt
@@ -0,0 +1,14 @@
+package com.alice.healthz.application
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+class HealthServiceTest {
+
+    @Test
+    fun `execute service heathz`(){
+        val service = HealthService()
+        assertEquals("ok",service.execute())
+    }
+}
